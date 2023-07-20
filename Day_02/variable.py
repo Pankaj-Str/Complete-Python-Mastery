d7c05f35d882f63f65806a93b606a00172d5d1b8
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-07-20 15:11:42 +0000

    Day_3

---
+++ Day_02/variable.py
@@ -0,0 +1,21 @@
+name = "joy"
+age = 12
+height = 3.5
+
+print("My Name is : ",name)
+print("My Age is : ",age)
+print("My height is : ",height)
+
+number1 = 100
+number2 = 200
+result = number1+number2
+print("Result is : - ",result)
+
+
+name = str(input("Enter Your Name "))
+age = int(input("Enter Your age "))
+height = float(input("Enter Your height "))
+
+print("My Name is : ",name)
+print("My Age is : ",age)
+print("My height is : ",height)
