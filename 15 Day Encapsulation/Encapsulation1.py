2fc3276c0096f3560f972a2b7b2c66490d1ad532
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-09-14 10:23:19 +0000

    update Days...

--- 15 Day Encapsulation/Encapsulation1.py
+++ 15 Day Encapsulation/Encapsulation1.py
@@ -1,32 +1,18 @@
-class info:
-    def __init__(self,name,age,height):
+class clientInfo:
+    def info(self,name,age,height):
         self.name = name
         self.age = age
         self.height = height
-    def PrintData(self):
-        print("Your Name is : ",self.name)
-        print("Your age is : ",self.age)
-        print("Your height is : ",self.height)
 
-    def getName(self):
-        return self.name  
-    def getAge(self):
+    def getname(self):
+        return self.name
+    def getage(self):
         return self.age
-    def getHeight(self):
-        return self.height   
-     
-    def setName(self,name):
-        self.name = name
-    def setAge(self,age):
-        self.age = age
-    def setHeight(self,height):
-        self.height = height 
-
-Info = info("joy",12,3.5)
-Info.PrintData()
-print(Info.getName())
-print(Info.getAge())
-print(Info.getHeight())
-
-Info.setName("Toy")
-Info.PrintData()
+    def getheight(self):
+        return self.height        
+    
+C_Info = clientInfo()
+C_Info.info("joy",12,3.4)
+print("My Name is ",C_Info.getname())
+print("My age is ",C_Info.getage())
+print("My height is ",C_Info.getheight())
---
+++ 16 Day Constructors/Constructors.py
@@ -0,0 +1,8 @@
+class ShopInfo():
+    def __init__(self):
+        print("Shop Name Joy Samosa Point")
+    def address(self):
+        print("dadar west --- ")   
+
+obj = ShopInfo()
+obj.address()
