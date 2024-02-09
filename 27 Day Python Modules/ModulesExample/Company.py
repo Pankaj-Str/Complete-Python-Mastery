16e414271b8a3cae7c58a5809d988dbd8c26e630
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-02-09 13:55:32 +0000

    update - modules

---
+++ 27 Day Python Modules/ModulesExample/Company.py
@@ -0,0 +1,12 @@
+def tds(salary,tds):
+    tds = salary*tds/100
+    salary = salary-tds
+    return salary
+
+def min(num1,num2,num3):
+    if num1 < num2 and num1 < num2:
+        print('Number ',num1,' is smallest. ') 
+    elif num2 < num1 and num2 < num3:
+        print('Number ',num2,' is smallest. ')
+    else : 
+        print('Number ',num3,' is smallest. ')       
---
+++ 27 Day Python Modules/ModulesExample/Employee.py
@@ -0,0 +1,15 @@
+# salary 78000
+# tds 12%
+
+import Company as c
+
+salary = 78000
+tds = 12
+
+
+final_salary = c.tds(salary,tds)
+
+print("Monthly Pay salary : ",final_salary)
+
+
+c.min(39,8,90)
