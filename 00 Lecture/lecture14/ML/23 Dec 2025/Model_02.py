f51ec0d09c99f1aa4012a93910fdf088a430064d
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-12-23 19:42:00 +0530

    Enhance Streamlit app with Random Forest Classifier setup and page design

--- 00 Lecture/lecture14/ML/23 Dec 2025/Model_02.py
+++ 00 Lecture/lecture14/ML/23 Dec 2025/Model_02.py
@@ -1,5 +1,25 @@
-#pip install streamlit
+# pip install streamlit
+# pip install scikit-learn
 import streamlit as st
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
-st.title("My First Streamlit App")
+# page design 
+st.set_page_config(
+    page_title="Random Forest Classifier",
+    page_icon="🌲",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
+
+st.title("💀Random Forest Classifier ☠️🌲☠️")
+st.markdown(
+    """
+    This app allows you to train a Random Forest Classifier on your dataset.
+    - CWPC 2025
+    - Developed by Your Name
+    ### GitHub: pankaj_str
+    """
+)
+
