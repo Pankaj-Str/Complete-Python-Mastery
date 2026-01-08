ca01339affb63ab228247b7a75eaf93391c6b53f
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-01-08 08:55:33 +0530

    Add files via upload

---
+++ 00 Lecture/lecture15/08 jan 2026/Function_2.py
@@ -0,0 +1,16 @@
+# function with return type
+x = 100
+print(x)
+
+def get_number():
+    return 900
+
+# print this function
+print(get_number())
+
+# function with arg/return type
+def add_number(n1,n2):
+    result = n1 + n2
+    return result
+
+print(add_number(90,80))
