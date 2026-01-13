5bf03b8c8fcb7d40b548ea2c7f59862119fa9649
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-01-13 08:25:48 +0530

    Add files via upload

---
+++ 00 Lecture/lecture15/13 Jan 2026/List_methods.py
@@ -0,0 +1,42 @@
+#List Methods
+#append()
+#extend()
+#insert()
+#remove()
+#pop()
+#clear()
+#index()
+#count()
+#sort()
+#reverse()
+#copy()
+
+data = [21,22,23,24,25,26]
+print(data)
+
+# print using loop
+for i in data:
+    print(i)
+
+#append()
+data.append(99)
+print(data)
+#extend()
+data.extend([22,33,44])
+print(data)
+#insert()
+data.insert(3,99)
+print(data)
+#remove()
+#data.remove(99)
+#print(data)
+#pop()
+data.pop()
+print(data)
+#clear()
+data.clear()
+#index()
+#count()
+#sort()
+#reverse()
+#copy()
