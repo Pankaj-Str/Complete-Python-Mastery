b1e3ddb5f910b6118f5955f95a1e11a65da47955
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-01-17 08:32:15 +0530

    Update dictionaries.py

--- 00 Lecture/lecture15/17 Jan 2026/dictionaries.py
+++ 00 Lecture/lecture15/17 Jan 2026/dictionaries.py
@@ -32,5 +32,27 @@ for i in data:
 del data['city']
 
 print(data)
-    
+
+
+#clear()
+# data.clear()
+#copy()
+d1 = data.copy()
+print(d1)
+#get()
+print(data.get('Name'))
+#items()
+print(data.items())
+#keys()
+print(data.keys())
+#values()
+print(data.values())
+#pop()
+data.pop('height')
+print(data)
+#update() # add new data
+data_new = {"course":"Java","Marks":56}
+data.update(data_new)
+print(data)
+
 
