3ad0e029edb9bf36cfd5b128ef05247f9744a1f1
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-01-20 08:38:54 +0530

    Add files via upload

---
+++ 00 Lecture/lecture15/20 Jan 2026/set.py
@@ -0,0 +1,50 @@
+data = {23,23,45,45,78,1,1,1,1}
+print(data)
+
+#data = set()
+#print(type(data))
+
+list_data = [1,1,12,2,3,3,4]
+
+set_data = set(list_data)
+print(set_data)
+
+
+# add()
+data.add(99)
+print(data)
+
+# add more update()
+ex_data = {22,33,22,44}
+data.update(ex_data)
+print(data)
+
+
+# remove()
+#data.remove(99)
+#print(data)
+
+# discard()
+#data.discard(45)
+#print(data)
+
+# pop()
+r_value = data.pop()
+print(r_value)
+print(data)
+
+# clear()
+data.clear()
+print(data)
+
+
+
+
+
+
+
+
+
+
+
+
