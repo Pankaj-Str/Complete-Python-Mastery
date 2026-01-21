dabca4f81368204252a92c3140cb294fbb32e020
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-01-21 20:54:00 +0530

    Add files via upload

---
+++ 00 Lecture/lecture16/21 Jan 2026/Basic.py
@@ -0,0 +1,117 @@
+# print() function
+print(900)
+print("cwpc")
+print('cwpc')
+
+print("'welcome'")
+print('"welcome"')
+
+print('my number is ',78678)
+
+# variable 
+x = 10
+print(x)
+x = 900
+print(x)
+
+# Example
+
+name = "Nishant"
+age = 23
+height = 4.5
+
+print('your name is : ',name)
+print('your age is : ',age)
+print('your height is : ',height)
+
+
+# type()
+print(type(name))
+print(type(age))
+print(type(height))
+
+
+# datatype
+x = 78
+print(type(x))
+x = 3.4
+print(type(x))
+x = "hello"
+print(type(x))
+x = 78,
+print(type(x))
+x = (78,)
+print(type(x))
+x = (78,76)
+print(type(x))
+x = [78,45]
+print(type(x))
+x = {78,56}
+print(type(x))
+x = True
+print(type(x))
+
+
+# input()
+
+name = str(input('Enter Your Name - '))
+age = int(input('Enter Your age - '))
+height = float(input('Enter Your height - '))
+
+print('your name is : ',name)
+print('your age is : ',age)
+print('your height is : ',height)
+
+# type()
+print(type(name))
+print(type(age))
+print(type(height))
+
+
+# Example:
+x = input('Enter Number 1')
+y = input('Enter Number 2')
+total = x + y
+print('total = ',total)
+
+
+#int()
+#float()
+#str()
+
+# Example:
+x = int(input('Enter Number 1'))
+y = int(input('Enter Number 2'))
+total = x + y
+print('total = ',total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
