c33382a78bc8a50b71772322c366eb5c49635d16
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-01-22 20:53:16 +0530

    Add files via upload

---
+++ 00 Lecture/lecture16/22 Jan 2026/Operators.py
@@ -0,0 +1,66 @@
+# Arithmetic Operators
+x = 567
+y = 13
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x**2)
+
+# Comparison Operators 
+print(x < y)
+print(x > y)
+print(x == y)
+print(x != y)
+print(x <= y)
+print(x >= y)
+
+# Logical Operators
+
+print(56 > 90 and 67 != 78)
+print(56 > 90 or 67 != 78)
+
+print(not 23 == 23)
+
+print(23 != 23)
+
+# Example
+print(23 < 90 and 56 >= 16 or 67 == 89 )
+
+# Assignment Operators
+x = 10
+print(x)
+
+# x = x + 10
+x += 10
+print(x)
+x *= 10
+print(x)
+x -= 10
+print(x)
+x /= 10
+print(x)
+
+#  Membership Operators
+data = "codeswithpankaj"
+print('c' in data)
+print('c' not in data)
+
+# Identity Operators
+data1 = "cwpc"
+data2 = "cwp"
+
+print(data1 is data2)
+print(data1 is not data2)
+
+print(data1 == data2)
+print(data1  != data2)
+
+
+
+
+
+
---
+++ 00 Lecture/lecture16/22 Jan 2026/if_else.py
@@ -0,0 +1,23 @@
+a1 = 10
+b1 = 20
+
+# find the smallest number - 
+
+if a1 < b1:
+    print("a1 is smallest...") # T
+else:
+    print("a1 is not smallest...")# F
+
+
+a = 10
+b = 9
+c = 8
+
+if a < b and a < c:
+    print('a is smallest ... ')
+elif b < a and b < c:
+    print("b is smallest ...")
+else:
+    print("C is smallest ...")
+
+
