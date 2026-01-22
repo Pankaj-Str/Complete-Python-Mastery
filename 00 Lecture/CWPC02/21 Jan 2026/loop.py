5d354cd5274da5fd8b1284db8f6aee4124241a6e
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-01-22 08:09:56 +0530

    update new data

---
+++ 00 Lecture/CWPC02/21 Jan 2026/loop.py
@@ -0,0 +1,21 @@
+word = "codeswithpankaj"
+for i in word:
+    print(i)
+
+# range(end) start = 0
+print('# range(end) start = 0')
+for i in range(10):
+    print(i)
+
+# range(start,end)
+print('# range(start,end)')
+
+for i in range(10,20):
+    print(i)
+
+# range(start,end,step)
+print('# range(start,end,step)')
+for i in range(10,50,5):
+    print(i,end=' ')
+
+    
