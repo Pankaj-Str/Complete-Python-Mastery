0b1cb53b99a59399f421483973e1ba40a7b01a22
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-02-04 20:45:56 +0530

    Add files via upload

---
+++ 00 Lecture/lecture16/27 jan 2026/loop.py
@@ -0,0 +1,17 @@
+# for i in range(100):
+#     if i == 11:
+#         break
+#     print(i,end=' ')
+# print("Stop")
+
+
+# for i in range(50):
+#     if i < 11:
+#         continue
+#     print(i,end="")
+
+for i in range(20):
+    if i%2 == 1:
+        continue
+    print(i,end=" , ")
+
