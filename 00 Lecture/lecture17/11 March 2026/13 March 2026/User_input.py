f5d5324f8f0e85e4bac35b516dcf9931e708c80f
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-03-13 08:25:53 +0530

    Add files via upload

---
+++ 00 Lecture/lecture17/11 March 2026/13 March 2026/User_input.py
@@ -0,0 +1,38 @@
+#input() -> user input
+
+# example 1
+
+Name = input('Enter Your Name = ')
+Age = input('Enter Your Age = ')
+print('Your Name is = ',Name)
+print('Your Age is = ',Age)
+
+# check type
+print(type(Name))
+print(type(Age))
+
+
+# example 2
+x = input('Enter Number 1 - ')
+y = input('Enter Number 2 - ')
+
+result = x + y
+print('this is result = ',result)
+
+
+# Type Conversion
+# int()
+# str()
+# float()
+
+x = int(input('Enter Number 1 - '))
+y = int(input('Enter Number 2 - '))
+
+result = x + y
+print('this is result = ',result)
+
+
+
+
+
+
