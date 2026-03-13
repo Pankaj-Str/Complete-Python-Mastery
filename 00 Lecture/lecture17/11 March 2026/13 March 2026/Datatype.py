0c0a9cae4e08f3276221afca07b7361db7888529
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-03-13 08:25:36 +0530

    Add Datatype.py to demonstrate type checking

---
+++ 00 Lecture/lecture17/11 March 2026/13 March 2026/Datatype.py
@@ -0,0 +1,32 @@
+# type() -> check datatype of value
+
+data = 23
+print(type(data))
+
+data = "codeswithpankaj.com"
+print(type(data))
+
+data = 2.3
+print(type(data))
+
+data = True
+print(type(data))
+
+
+data = 23,
+print(type(data))
+
+data = (23,)
+print(type(data))
+
+data = (23,45)
+print(type(data))
+
+data = [23,24,25,26,27]
+print(type(data))
+
+data = {34,34,35}
+print(type(data))
+
+data = {'name':'Nishant Jain','Age':25}
+print(type(data))
