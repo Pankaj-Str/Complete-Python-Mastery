c51f8b644c41b0eb95d12ba7ecea735939d15f7e
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-03-17 12:27:27 +0530

    update lecture

---
+++ 00 Lecture/lecture17/17 March 2026/basic_01.py
@@ -0,0 +1,57 @@
+#age = int(input('Enter Your Age '))
+#height = float(input('Enter Your Height '))
+#BMI = age / height
+#print(BMI)
+
+x = 190
+y = 8
+print(x+y)
+print(x-y)
+print(x*y)
+print(x**2)
+print(x/y)
+print(x//y)
+print(x%y)
+
+
+print(x == y)
+print(x != y)
+print(x < y)
+print(x > y)
+print(45 >= 45)
+print(45 <= 45)
+
+x = 10
+#print(x)
+# x = x + 10
+x += 10
+print(x)
+
+x *= 10
+print(x)
+
+x /= 10
+print(x)
+
+x -= 10
+print(x)
+
+
+print(12 < 90 and 56 != 56)
+
+print(12 < 90 or 56 != 56)
+
+print(not 23 == 23)
+
+data1 = "cwpc"
+print("c" in data1)
+print("c" not in data1)
+
+data2 = "CWPP"
+print(data1 is data2)
+print(data1 is not data2)
+
+
+
+
+               
