e0927651761553220446e628a6761fe3c9a00753
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-03-18 08:50:22 +0530

    update if else lecture ...

---
+++ 00 Lecture/lecture17/18 March 2026/if_else.py
@@ -0,0 +1,21 @@
+#a = int(input("Enter Number 1 "))
+#b = int(input("Enter Number 2 "))
+
+#if a < b :
+#    print("number 1 is smallest")
+#else:
+#    print("number 1 is not smallest")
+
+
+a = 10
+b = 9
+c = 2
+
+if a < b and a < c:
+    print("A is smallest ...")
+elif b < a and b < c:
+    print("B is smallest ...")
+else:
+    print("C is smallest ..")
+    
+        
