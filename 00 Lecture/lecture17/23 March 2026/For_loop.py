2dd15f2229cc786d1e1d395a77ee682cae4863f4
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-03-23 09:02:23 +0530

    update - Lecture Loop Codes

---
+++ 00 Lecture/lecture17/23 March 2026/For_loop.py
@@ -0,0 +1,46 @@
+print("--- range(end_value)-----")
+for i in range(10):
+    print(i)
+
+
+print("--- range(start_value,end_value)-----")
+
+for i in range(11,22):
+    print(i)
+
+
+print("--- range(start_value,end_value,steps)-----")
+
+for i in range(10,50,5):
+    print(i)
+
+
+print('--------------------------------')
+
+
+number = int(input('Enter Number - '))
+
+for i in range(1,11):
+    print(number,' X ',i,' = ',i*number)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
