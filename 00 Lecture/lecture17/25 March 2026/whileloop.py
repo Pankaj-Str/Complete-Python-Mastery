2972dcf69a3620eab3e87f44bd0fe4beb1f98ead
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-03-25 09:21:03 +0530

    Create whileloop.py

---
+++ 00 Lecture/lecture17/25 March 2026/whileloop.py
@@ -0,0 +1,57 @@
+#start point
+#end point
+#output
+#+1/-1
+
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+# table = 2 X 1 = 2
+
+print("----------------------")
+
+a = 1
+while a <= 10:
+    print("2 X ",a," = ",a*2)
+    a = a + 1
+
+
+
+print("-------------------------")
+
+char = input('Enter Char = ')
+
+if char == 'a' or char == 'e' or char =='i' or char =='o' or char == 'u' or char == 'A' or char == 'E' or char =='I' or char =='O' or char == 'U':
+    print("vowel ",char)
+else:
+    print("Not vowel ",char)
+
+
+print("----------------------")
+
+word = input('Enter word ')
+if 'i' in word or 'a' in word or 'o' in word or 'u' in word or 'e' in word :
+    print("vowel ",word)
+else:
+    print("Not vowel ",word)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
