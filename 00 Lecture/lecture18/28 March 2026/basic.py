6f92df772b2fbd8668d8b54bd655200373d7d30d
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-03-28 11:26:11 +0530

    update basic

---
+++ 00 Lecture/lecture18/28 March 2026/basic.py
@@ -0,0 +1,42 @@
+# hello
+
+print(2000)
+print("welcome")
+print('welcome to codeswithpankaj.com')
+print('my number is ',9876543212)
+
+
+x = 100
+print(x)
+
+a , b = 10,20
+
+print(a)
+print(b)
+
+# input()
+
+x = input('Enter your number ')
+print('this is your number = ',x)
+
+name = input('Enter Your Name = ')
+age = input('enter your age = ')
+print('your name is ',name)
+print('your age is ',age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
