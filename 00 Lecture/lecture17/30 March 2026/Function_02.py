f4e7d81046158257b6ad116f479875b8e8fbfda5
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-03-30 08:45:23 +0530

    Add Function_02.py with basic functions and prints

---
+++ 00 Lecture/lecture17/30 March 2026/Function_02.py
@@ -0,0 +1,40 @@
+# return type
+def get_number():
+    return 100
+
+print(get_number())
+
+x = get_number()
+print(x)
+
+
+def add():
+    result = 10+90
+    return result
+
+print(add())
+# parameters with return type
+def result(a,b):
+    result = a + b
+    return result
+
+print(result(23,56))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
