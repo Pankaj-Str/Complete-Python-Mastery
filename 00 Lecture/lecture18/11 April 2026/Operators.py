1d8cee5b2a545ae4ab9974d2beb5aa98552b9a09
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-04-11 11:58:09 +0530

    update data

---
+++ 00 Lecture/lecture18/11 April 2026/Operators.py
@@ -0,0 +1,66 @@
+x = 113
+y = 45
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** 3)
+
+
+print(x == y)
+print(x != y)
+print(x < y)
+print(x > y)
+print(x <= y)
+print(x >= y)
+
+# Assignment Operators
+
+
+x = 10
+print(x)
+
+# x = x + 10
+x +=10
+print(x)
+
+# and
+
+print(10 < 90 and 78 == 67)
+
+# or
+print(10 < 90 or 78 == 67)
+
+# not
+print(not 10 == 10)
+
+# Membership Operators
+data = "codeswithpankaj.com"
+
+print('a' in data)
+print('a' not in data)
+
+data1 = "cwpc"
+data2 = "cwp"
+# Identity Operators
+print(data1 is data2)
+print(data1 == data2)
+
+print(data1 is not data2)
+print(data1 != data2)
+
+
+
+
+
+
+
+
+
+
+
+
+
