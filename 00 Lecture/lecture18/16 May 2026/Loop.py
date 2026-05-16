d91b20f4885864d5242f2efe1a6104934630e81d
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-05-16 11:44:31 +0530

    update new data ...

---
+++ 00 Lecture/lecture18/16 May 2026/Loop.py
@@ -0,0 +1,23 @@
+data = "welcome"
+for i in data:
+    print(i)
+
+print("--------for loop-------")
+
+# range(stop point) start 0 end = stop point
+for i in range(10): # stop point 9
+    print(i)
+
+print("--------for loop-------")
+
+# range(start point , stop point)
+
+for i in range(11,19):
+    print(i)
+
+print("--------for loop-------")
+
+# range(start point , stop point , step point)
+
+for i in range(10,21 , 2):
+    print(i)
