e6906c4e8811cd96c718f35857ecc7083b441c5c
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-06-04 11:25:56 +0530

    update new lecture

---
+++ 00 Lecture/lecture19/04 June 2026/While_loop.py
@@ -0,0 +1,10 @@
+# start point
+# end point
+# output
+# +1 , -1
+
+x = 1
+while x <= 10:
+    print(x)
+    x = x + 1
+
---
+++ 00 Lecture/lecture19/04 June 2026/user_input_datatype.py
@@ -0,0 +1,57 @@
+# input()
+
+#name = input('enter your name ')
+#age = input('enter your age ')
+#height = input('enter your height ')
+
+#print("your name is : ",name)
+#print("your age is :",age)
+#print("your height is :",height)
+
+# int()
+# str()
+# float()
+
+x = int(input('enter number1 '))
+y = int(input('enter number2 '))
+
+z = x + y
+print("this is result ",z)
+
+# check type
+print(type(x))
+print(type(y))
+
+
+
+
+------------------
+Enter Your Name = ?
+Enter your Roll Number = ?
+Enter Java Marks 100/?
+Enter C Marks 100/?
+Enter Python Marks 100/?
+Enter R Marks 100/?
+Enter SQL Marks 100/?
+
+--- total = ?
+--- per % = ?
+--- Grade =
+A = per 70-100
+B = 60 - 70
+c = 40 - 60
+D = 30 - 40
+fail = 0 - 30
+
+
+
+
+
+
+
+
+
+
+
+
+
