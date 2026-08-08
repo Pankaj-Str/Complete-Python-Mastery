ca974a6dcede0c775c4fea07586640d08204d50d
Pankaj Chouhan <pankajchouhan80@gmail.com>
2026-08-08 12:35:11 +0530

    Add initial version of basic.py with variable examples and print statements

---
+++ 00 Lecture/lecture23/basic.py
@@ -0,0 +1,19 @@
+print("welcome....")
+print(98766)
+
+
+# variable in python
+
+number = 100
+
+print("this is my number = ",number)
+
+name = "Joy Jain"
+height = 4.5
+city = "mumbai"
+age = 34
+
+print("your name is : ",name)
+print("your height is : ",height)
+print("your city is ",city)
+print("your age is : ",age)
