692a258272b89c0fde0e23aa05e64d5af49278b9
Pankaj Chouhan (Code with Pankaj) <pankajchouhan80@gmail.com>
2026-08-22 11:52:03 +0530

    Create variable and datatype demonstration script
    Added a Python script to demonstrate variables, data types, and user input.

---
+++ 00 Lecture/lecture24/variable and datatype.py
@@ -0,0 +1,53 @@
+print("welcome")
+
+# variable
+
+x = 100
+print(x*9)
+
+name = "priti"
+age = 21
+height = 5.9
+
+print("your name is ",name)
+print("your age is ",age)
+print("your height is ",height)
+
+# check Datatype
+print(type(name))
+print(type(age))
+print(type(height))
+
+# input()
+# name = input("Enter Your name - ")
+# age = input("Enter Your age - ")
+# height = input("Enter Your height - ")
+
+# print("your name is ",name)
+# print("your age is ",age)
+# print("your height is ",height)
+
+# check Datatype
+print(type(name))
+print(type(age))
+print(type(height))
+
+# int()
+# float()
+# str()
+# example
+x = int(input("Enter Number 1 "))
+y = int(input("Enter Number 2 "))
+result = x + y
+print("this is result ",result)
+
+
+
+
+
+
+
+
+
+
+
