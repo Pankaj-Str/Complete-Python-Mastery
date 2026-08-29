95ff6dcd730d4e5b951b13b6597937d7a89c39e8
Pankaj Chouhan (Code with Pankaj) <pankajchouhan80@gmail.com>
2026-08-29 12:43:16 +0530

    Create Operators.py with examples of operators
    Added a new Python file demonstrating various operators.

---
+++ 00 Lecture/lecture24/Operators.py
@@ -0,0 +1,66 @@
+# Arithmetic Operators
+x = 987
+y = 67
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x ** 3)
+print(x % y)
+
+# Comparison Operators
+print(34 == 134)
+print(34 != 134)
+
+print(34 < 134)
+print(34 > 134)
+
+print(34 <= 34)
+
+print(34 >= 134)
+
+# Assignment Operators
+
+a = 23
+print(a)
+# a = a + 10
+a += 10
+print(a)
+
+# Logical Operators
+# and 
+print(45 < 67 and 67 != 67)
+# or
+print(45 < 67 or 67 != 67)
+# not
+print(not 34 == 34)
+print(34 != 34)
+
+
+# Membership Operators
+
+data = "codeswithpankaj"
+
+print("w" in data)
+print("w" not in data)
+
+# Identity Operators
+data1 = "cwpc"
+data2 = "codeswithpankaj"
+
+print(data1 is data2)
+
+print(data1 is not data2)
+
+
+
+
+
+
+
+
+
+
+
