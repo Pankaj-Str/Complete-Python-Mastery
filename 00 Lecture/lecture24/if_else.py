1208237615638412bc20e353c745550fdc82e8f4
Pankaj Chouhan (Code with Pankaj) <pankajchouhan80@gmail.com>
2026-09-12 12:05:11 +0530

    update New Lecture ....

---
+++ 00 Lecture/lecture24/if_else.py
@@ -0,0 +1,35 @@
+print(23 == 45)
+print(23 < 45)
+
+a = 34
+b = 90
+
+# find the smallest number
+
+if(a < b):
+    # True
+    print("a is smallest ")
+else:
+    # False
+    print("a is not smallest ")
+    
+# find the number +ve / -ve
+
+number = 91
+
+if(number < 0):
+    print("-ve")
+else:
+    print("+ve")
+
+if(number%2 == 0):
+    print("even")
+else:
+    print("odd")
+        
+
+
+
+
+    
+
