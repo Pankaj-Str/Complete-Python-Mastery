a046cb8724f5e6e92dd698b4a39a768cc2de99c3
Pankaj Chouhan (Code with Pankaj) <pankajchouhan80@gmail.com>
2026-09-19 11:53:49 +0530

    update new codes....

---
+++ 00 Lecture/lecture24/Loop.py
@@ -0,0 +1,21 @@
+# for range(end)
+
+for i in range(10):
+    print(i)
+
+
+# range(start,end)
+print("-------------------")
+for i in range(10,20):
+    print(i)
+
+# range(start,end,steps)
+print("-------------------")
+for i in range(10,30,2):
+    print(i)
+
+
+print("-------------------")
+for i in range(1,20):
+    print(i*7,end=" | ")    
+
